7a4808574cbbef44d4c18bfd4798697a40dfc9de
7theturtle <samy.srzcln@gmail.com>
2023-10-11 17:02:42 -0600

    Commit Reacomodacion de los archivos y creacion de la navegacion

---
+++ app/src/main/java/com/example/pygtianguistraker/core/Helper.kt
@@ -0,0 +1,4 @@
+package com.example.pygtianguistraker.core
+
+object Helper {
+}
--- app/src/main/java/com/example/pygtianguistraker/ui/view/LoginActivity.kt
+++ app/src/main/java/com/example/pygtianguistraker/ui/view/LoginActivity.kt
@@ -1,11 +1,12 @@
-package com.example.pygtianguistraker
+package com.example.pygtianguistraker.ui.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.pygtianguistraker.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.login_activity)
     }
 }
