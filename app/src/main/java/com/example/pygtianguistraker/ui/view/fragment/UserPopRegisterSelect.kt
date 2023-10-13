e93d006323d9c195eeb08d1d2f02d58d7b0cfd3e
SAMUEL <samy.srzcln@gmail.com>
2023-10-13 01:21:07 -0600

    Creacion de fragments y login terminados

---
+++ app/src/main/java/com/example/pygtianguistraker/ui/view/fragment/UserPopRegisterSelect.kt
@@ -0,0 +1,40 @@
+package com.example.pygtianguistraker.ui.view.fragment
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.DialogFragment
+import com.example.pygtianguistraker.R
+import com.example.pygtianguistraker.ui.view.RegisterUserActivitySeller
+import com.example.pygtianguistraker.ui.view.RegisterUserActivitybuyer
+
+class UserPopRegisterSelect : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_user_pop_register_select, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        val ShowActivityRegisterSeller = view.findViewById<Button>(R.id.registerBuyer)
+        val ShowActivityRegisterBuyer = view.findViewById<Button>(R.id.registerSeller)
+
+        ShowActivityRegisterSeller.setOnClickListener {
+            val intent = Intent(requireContext(),RegisterUserActivitySeller ::class.java)
+            startActivity(intent)
+            dismiss()
+        }
+        ShowActivityRegisterBuyer.setOnClickListener {
+            val intent = Intent(requireContext(), RegisterUserActivitybuyer::class.java)
+            startActivity(intent)
+            dismiss()
+        }
+    }
+
+}
