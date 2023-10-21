650ff8072f5c2b73e010211fa23114f22fd6ea47
SAMUEL <samy.srzcln@gmail.com>
2023-10-20 22:41:08 -0600

    Creacion de fragments y login terminados y navegacion inicial

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,6 @@ android {
 }
 
 dependencies {
-
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
@@ -54,4 +53,5 @@ dependencies {
     implementation("androidx.fragment:fragment-ktx:1.6.1")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
 }
