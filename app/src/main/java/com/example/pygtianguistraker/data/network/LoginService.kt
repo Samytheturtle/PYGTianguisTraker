fb86c4449888bab9150cfebcae593dca70c8a08f
7theturtle <samy.srzcln@gmail.com>
2023-10-31 16:48:57 -0600

    Cambio del manifest

--- app/src/main/java/com/example/pygtianguistraker/data/network/LoginService.kt
+++ app/src/main/java/com/example/pygtianguistraker/data/network/LoginService.kt
@@ -7,12 +7,5 @@ import kotlinx.coroutines.withContext
 import retrofit2.Retrofit
 
 class LoginService {
-    private val retrofit = Helper.getRetrofit()
 
-    suspend fun loginUser(email: String, password: String): Int {
-        return withContext(Dispatchers.IO) {
-            val response = retrofit.create(LoginApiClient::class.java).loginUser(email, password)
-            response.body() ?:-1  // Puedes cambiar el valor predeterminado (-1) según tus necesidades
-        }
-    }
 }
