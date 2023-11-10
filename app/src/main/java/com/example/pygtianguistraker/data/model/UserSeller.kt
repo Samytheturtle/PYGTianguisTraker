8d6d398ffbd0edfd8afba2c174f5c04e45da4086
ElderBike4 <78830446+ElderBike4@users.noreply.github.com>
2023-11-10 13:38:46 -0700

    Pyg tianguis trajer v beta 1.0 emg (#9)
    * Registro de usuario vendedor funcionalidad primera parte
    
    * Actualización de URL para acceder a la api, cambiando configuración de red y agregando registro vendedor
    
    * Registro de usuario

--- app/src/main/java/com/example/pygtianguistraker/data/model/UserSeller.kt
+++ app/src/main/java/com/example/pygtianguistraker/data/model/UserSeller.kt
@@ -1,4 +1,14 @@
 package com.example.pygtianguistraker.data.model
 
-class UserSeller {
+data class UserSeller (var idVendedor : Int,
+                       var nombreVendedor: String, var calificacionVendedor: String,
+                       var correoVendedor: String, var contraseniaVendedor: String, var horarioLunesVendedor: String,
+                       var horarioMartesVendedor: String, var horarioMiercolesVendedor:String, var horarioJuevesVendedor:String,
+                       var horarioViernesVendedor: String, var horarioSabadoVeneddor: String, var horarioDomingoVendedor: String,
+                       var idTianguisVendedor: Int, var fechaNacimientoVendedor: String){
+    constructor() : this(
+        0, "", "", "", "", "",
+        "", "", "", "", "", "",
+        0, ""
+    )
 }
--- app/src/main/java/com/example/pygtianguistraker/ui/view/RegisterUserActivitySeller.kt
+++ app/src/main/java/com/example/pygtianguistraker/ui/view/RegisterUserActivitySeller.kt
@@ -1,21 +1,46 @@
 package com.example.pygtianguistraker.ui.view
 
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.widget.DatePicker
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.pygtianguistraker.data.model.UserSeller
 import com.example.pygtianguistraker.databinding.RegisterActivitySellerBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class RegisterUserActivitySeller : AppCompatActivity(){
     private lateinit var binding:RegisterActivitySellerBinding
+    private lateinit var message :String
+    private val calendar = Calendar.getInstance()
+    private lateinit var name:String
+    private lateinit var email:String
+    private lateinit var password:String
+    private lateinit var date:String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = RegisterActivitySellerBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         binding.btNextSeller.setOnClickListener{
-            val intent= Intent(this, RegisterUserActivityTianguis::class.java)
-            startActivity(intent)
+            if(!validData()){
+                Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+            }else{
+                if(validPassword()){
+                    val user = UserSeller(0,name,"",email,password,"","","","","","","",0,date)
+                    val intent= Intent(this, RegisterUserActivityTianguis::class.java)
+                    intent.putExtra("nombreVendedor",user.nombreVendedor);
+                    intent.putExtra("correo",user.correoVendedor)
+                    intent.putExtra("date",user.fechaNacimientoVendedor)
+                    intent.putExtra("password",user.contraseniaVendedor)
+                    startActivity(intent)
+                }else{
+                    Toast.makeText(this,message,Toast.LENGTH_SHORT).show()
+                }
+            }
         }
 
         binding.btCancelSeller.setOnClickListener{
@@ -23,6 +48,67 @@ class RegisterUserActivitySeller : AppCompatActivity(){
             startActivity(intent)
 
         }
+
+        binding.etBirthdate.setOnClickListener(){
+            showDatePickerDialog()
+        }
+
+    }
+    private fun validData(): Boolean{
+        name = binding.etName.text.toString().trim()
+        email = binding.etEmail.text.toString().trim()
+        date = binding.etBirthdate.text.toString().trim()
+        password = binding.etPassword.text.toString().trim()
+        var validation: Boolean
+        validation = true
+        if(name.isEmpty()){
+            validation = false
+            message = "El nombre no puede estar vacio"
+        }
+        if(email.isEmpty()){
+            validation = false
+            message = "El correo no puede estar vacio"
+        }
+        if(date.isEmpty()){
+            validation = false
+            message = "La fecha de nacimiento no puede estar vacia"
+        }
+        if(password.isEmpty()) {
+            validation = false
+            message = "La contraseña no puede estar vacia"
+        }
+        return validation
     }
 
+    private fun validPassword(): Boolean{
+        var validation: Boolean
+        validation = false
+        message = "Las contraseñas no son iguales"
+        var confirmPassword = binding.etConfirmPassword.text.toString().trim()
+        if(binding.etPassword.text.toString().trim().equals(confirmPassword)){
+            validation = true
+        }
+
+        return validation
+    }
+
+
+    private fun showDatePickerDialog() {
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { _: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+                val selectedDate = Calendar.getInstance()
+                selectedDate.set(year, month, dayOfMonth)
+                updateDateInView(selectedDate)
+            },
+            calendar.get(Calendar.YEAR),
+            calendar.get(Calendar.MONTH),
+            calendar.get(Calendar.DAY_OF_MONTH)
+        )
+        datePickerDialog.show()
+    }
+    private fun updateDateInView(calendar: Calendar) {
+        val dateFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+        binding.etBirthdate.setText(dateFormat.format(calendar.time))
+    }
 }
