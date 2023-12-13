3fc30c376fc3b2bef0f6194c87eedd9942675d0a
FranciscoXavvier <franciscoxavieram@gmail.com>
2023-12-12 21:22:52 -0600

    Recycler view

--- app/src/main/java/com/example/pygtianguistraker/core/Helper.kt
+++ app/src/main/java/com/example/pygtianguistraker/core/Helper.kt
@@ -6,7 +6,9 @@ object Helper {
     fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             //Cambien la ip cada que quieran ejecutarlo, la ip es la de sus computadoras.
-            .baseUrl("http://192.168.1.102:3000")
+
+            .baseUrl("http://192.168.100.74:3000")
+
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
